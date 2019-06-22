ecf66ff79758b2548513e10218e2324be5af5dad
wurikiji <wurikiji@gmail.com>
2019-06-22 18:47:06 +0900

    제목 적는곳 얼라인 안 맞는거 수정함.

--- lib/src/screen/make_goal/view/module/define_title.dart
+++ lib/src/screen/make_goal/view/module/define_title.dart
@@ -8,30 +8,33 @@ class DefineTitle extends StatelessWidget {
   Widget build(BuildContext context) {
     return QuestionScaffold(
       title: '프로젝트의 상세 타이틀을 적어주세요.',
-      body: TextField(
-        decoration: InputDecoration(
-          filled: true,
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(4.0),
-            borderSide: BorderSide(style: BorderStyle.none),
-          ),
-          fillColor: Color(0xff2b2b2b),
-          hintText: '주 3회 이상 운동하기',
-          hintStyle: DoitMainTheme.makeGoalHintTextStyle,
-          contentPadding:
-              EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 10.0),
+        decoration: ShapeDecoration(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(4.0)),
+          color: Color(0xff2b2b2b),
         ),
-        style: DoitMainTheme.makeGoalUserInputTextStyle,
-        maxLines: 1,
-        onChanged: (String title) async {
-          FirstPageMakeGoalBloc _bloc = FirstPageMakeGoalBloc.getBloc(context);
-          _bloc.dispatch(
-            FirstPageMakeGoalInfoEvent(
-              action: FirstPageMakeGoalInfoAction.setTitle,
-              data: title,
-            ),
-          );
-        },
+        child: TextField(
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: '주 3회 이상 운동하기',
+            hintStyle: DoitMainTheme.makeGoalHintTextStyle,
+            contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+          ),
+          maxLines: 1,
+          style: DoitMainTheme.makeGoalUserInputTextStyle,
+          onChanged: (String title) async {
+            FirstPageMakeGoalBloc _bloc =
+                FirstPageMakeGoalBloc.getBloc(context);
+            _bloc.dispatch(
+              FirstPageMakeGoalInfoEvent(
+                action: FirstPageMakeGoalInfoAction.setTitle,
+                data: title,
+              ),
+            );
+          },
+        ),
       ),
     );
   }
